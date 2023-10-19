96156a4acf419b324ec49b5b8492c2aa5f080143
alfalana <106816657+alfalana@users.noreply.github.com>
2023-10-19 11:23:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,4 +8,31 @@ function tocaSom (seletorAudio) {
         //alert(`Elemento não encontrado`);
         console.log(`elemento não encontrado ou eletor inválido`);
     }
+
+}
+
+const ListaDeTeclas = document.querySelectorAll(´.tecla´);
+
+//para
+for(let contador< ListaDeTeclas.length; contador++){
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classlist[1];
+    const idAudio = ´#som_${instrumento}´; //yemplate tring  
+
+    tecla.onclick = funtion (evento) {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = funtion (){
+
+        if (evento.code === ´space´ || evento.code === ´enter´) {
+            tecla.classlist.add(´ativa´);
+        }
+
+    }
+
+    tecla.onkeyup = funtion (){
+        tecla.classlist.remove(´ativa´);
+    }
 }
